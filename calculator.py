c21ce18d38f01c3ddaf7c03ec10890a3b044663b
alireza822 <alireza822@gmail.com>
2019-03-31 02:23:16 -0400

    flake and pytest edited 2

--- calculator.py
+++ calculator.py
@@ -8,6 +8,3 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
-
-
-    
--- test_calculator.py
+++ test_calculator.py
@@ -11,4 +11,3 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-
