1d142b91961e38734f15dcfb3c85e769b710813d
alireza822 <alireza822@gmail.com>
2019-03-31 02:52:27 -0400

    added division

--- calculator.py
+++ calculator.py
@@ -8,3 +8,7 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term/second_term
