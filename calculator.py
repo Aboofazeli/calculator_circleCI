5eb69bc310889962323ae436f11d72d29c4cbc08
alireza822 <alireza822@gmail.com>
2019-03-30 22:27:12 -0400

    calculator app

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+    
