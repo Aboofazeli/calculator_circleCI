c4ff3827abcab9f4c67a0df4d247df98c5b2f304
alireza822 <alireza822@gmail.com>
2019-03-31 02:19:37 -0400

    flake and pytest edited

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,14 @@
 import calculator
 
+
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
